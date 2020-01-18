1d30ecb4c5d342a51158116accd6a596502227d9
freeman1s1 <stugay@mail.ru>
2020-01-18 15:21:16 +0300

    producer2actvities

---
+++ TalentShow/app/src/main/java/com/example/talentshow/producer/ActivityProducerName.java
@@ -0,0 +1,14 @@
+package com.example.avatar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityProducerName extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_producer_name);
+    }
+}
