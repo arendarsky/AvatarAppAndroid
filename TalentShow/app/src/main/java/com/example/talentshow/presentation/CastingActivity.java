5ab9718836771a3152b8feabe5529833349959ae
freeman1s1 <stugay@mail.ru>
2020-01-20 22:21:15 +0300

    castingActivity

---
+++ TalentShow/app/src/main/java/com/example/talentshow/presentation/CastingActivity.java
@@ -0,0 +1,16 @@
+package com.example.talentshow.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.talentshow.R;
+
+public class CastingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_casting);
+    }
+}
