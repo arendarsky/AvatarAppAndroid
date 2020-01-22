21a88df6dc17eef5795cadac1fe3f687b5a6b94e
freeman1s1 <stugay@mail.ru>
2020-01-22 15:32:53 +0300

    checklibmedia

--- TalentShow/app/src/main/java/com/example/talentshow/presentation/CastingActivity.java
+++ TalentShow/app/src/main/java/com/example/talentshow/presentation/CastingActivity.java
@@ -2,15 +2,37 @@ package com.example.talentshow.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.example.talentshow.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import life.knowledge4.videotrimmer.K4LVideoTrimmer;
+import life.knowledge4.videotrimmer.interfaces.OnTrimVideoListener;
+
 public class CastingActivity extends AppCompatActivity {
 
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_casting);
+        /*K4LVideoTrimmer videoTrimmer = (K4LVideoTrimmer) findViewById(R.id.activity_casting_video);
+        videoTrimmer.setOnTrimVideoListener(new OnTrimVideoListener() {
+            @Override
+            public void getResult(Uri uri) {
+                if (videoTrimmer != null) {
+                    //videoTrimmer.setVideoURI(uri);
+                }
+            }
+
+            @Override
+            public void cancelAction() {
+
+            }
+        });*/
     }
 }
