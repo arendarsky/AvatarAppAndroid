1ae30d1aad12fc5a7560cbeb200f4e57b6bf6281
freeman1s1 <stugay@mail.ru>
2020-01-28 00:06:14 +0300

    CastingActivityChanges

--- TalentShow/app/src/main/java/com/example/talentshow/presentation/CastingActivity.java
+++ TalentShow/app/src/main/java/com/example/talentshow/presentation/CastingActivity.java
@@ -1,34 +1,61 @@
 package com.example.talentshow.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
+import com.example.talentshow.App;
 import com.example.talentshow.R;
+import com.google.android.material.snackbar.Snackbar;
+
+import javax.inject.Inject;
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import life.knowledge4.videotrimmer.K4LVideoTrimmer;
+import life.knowledge4.videotrimmer.interfaces.OnTrimVideoListener;
+import toothpick.Toothpick;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 
 public class CastingActivity extends AppCompatActivity {
 
+    @Inject
+    Context appContext;
 
-    
+    @BindView(R.id.activity_casting_video)
+    VideoView video;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_casting);
-        /*K4LVideoTrimmer videoTrimmer = (K4LVideoTrimmer) findViewById(R.id.activity_casting_video);
-        videoTrimmer.setOnTrimVideoListener(new OnTrimVideoListener() {
-            @Override
-            public void getResult(Uri uri) {
-                if (videoTrimmer != null) {
-                    //videoTrimmer.setVideoURI(uri);
-                }
-            }
+        ButterKnife.bind(this);
+        Toothpick.inject(this, Toothpick.openScope(App.class));
 
-            @Override
-            public void cancelAction() {
+        String url = "http://techslides.com/demos/sample-videos/small.mp4";
 
-            }
-        });*/
+        MediaController controls = new MediaController(this);
+        controls.setAnchorView(video);
+        video.setMediaController(controls);
+        video.setVideoURI(Uri.parse(url));
+        //video.setVideoPath(videoName1);
+        video.start();
     }
+
+
 }
