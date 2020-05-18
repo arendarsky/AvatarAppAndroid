81943cbd77894a11153ea50859d50f406711ca0f
freeman1s1 <stugay@mail.ru>
2020-05-18 11:46:47 +0400

    rewrite strings

--- TalentShow/app/src/main/java/com/avatar/ava/presentation/main/fragments/profile/profileSettings/ProfileSettingsFragment.java
+++ TalentShow/app/src/main/java/com/avatar/ava/presentation/main/fragments/profile/profileSettings/ProfileSettingsFragment.java
@@ -2,11 +2,13 @@ package com.avatar.ava.presentation.main.fragments.profile.profileSettings;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -46,6 +48,9 @@ public class ProfileSettingsFragment extends MvpAppCompatFragment implements Pro
     @BindView(R.id.fragment_profile_settings_email_edit)
     EditText email;
 
+    @BindView(R.id.fragment_profile_settings_info)
+    TextView info;
+
     private MainScreenActivity activity;
 
     public ProfileSettingsFragment() {
@@ -73,6 +78,7 @@ public class ProfileSettingsFragment extends MvpAppCompatFragment implements Pro
         Toothpick.inject(this, Toothpick.openScope(App.class));
         presenter.showEmail();
         email.setEnabled(false);
+        info.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     @OnClick(R.id.fragment_profile_settings_password)
